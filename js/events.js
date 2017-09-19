b7449aed08dae803919cac2c257ae91acf8dc974
Seth Barden <sethbarden@gmail.com>
2017-09-19 18:20:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(i) {
+    i.addClass("tasty");
+
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which === 47) {
+      alert('You have pressed a G';)
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
-
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
