e911c0a3992a549ffbfca0293230af9a9e6fffef
Seth Barden <sethbarden@gmail.com>
2017-09-19 18:35:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt() {
 
 function pressIt() {
   $("#typing").on('keydown', function(key) {
-    if (key.which === 47) {
+    if (key.which === 71) {
       alert('You have pressed a G');
     }
   });
