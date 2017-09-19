273e9596dab134c2fd4acfddf29568e4c3643c3a
Seth Barden <sethbarden@gmail.com>
2017-09-19 18:24:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,21 +9,21 @@ function frameIt() {
   $('img').on('load', function(i) {
     i.addClass("tasty");
 
-  })
+  });
 }
 
 function pressIt() {
   $('input').on('keydown', function(key) {
     if(key.which === 47) {
-      alert('You have pressed a G';)
+      alert('You have pressed a G');
     }
-  })
+  });
 }
 
 function submitIt() {
   $('form').on('submit', function() {
-    alert('Your form is going to be submitted now.')
-  })
+    alert('Your form is going to be submitted now.');
+  });
 }
 
 $(document).ready(function(){
